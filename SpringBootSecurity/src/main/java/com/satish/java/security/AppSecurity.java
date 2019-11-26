94fc62dcace46b72b2cddf30ae3aee178b30ee7d
Satish Kumar <satishbbly@gmail.com>
2019-11-26 21:10:34 +0530

    some modification

--- SpringBootSecurity/src/main/java/com/satish/java/security/AppSecurity.java
+++ SpringBootSecurity/src/main/java/com/satish/java/security/AppSecurity.java
@@ -33,7 +33,7 @@ public class AppSecurity extends WebSecurityConfigurerAdapter {
 		
 		http
 			.csrf().disable()
-			.authorizeRequests().antMatchers("/login").permitAll()
+			.authorizeRequests().antMatchers("/").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
